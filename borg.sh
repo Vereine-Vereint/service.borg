6984c9b6825edfcffa742741ad9710b07161d2ad
Johannes Schmid <Johannes Schmid>
2024-03-14 20:42:10 +0100

    v0.1

---
+++ borg.sh
@@ -0,0 +1,72 @@
+BORG_VERSION="v0.1"
+
+BORG_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+echo "[BORG] $BORG_VERSION ($(cd $BORG_DIR && git rev-parse --short HEAD))"
+
+declare -A borg_commands=(
+  [init]=":Create remote repository"
+  [info]=":Show repository information"
+  [list]=":Lists all backups"
+
+  [backup]="<name>:Backup current data"
+  [restore]="<name>:Restore data from backup"
+  [export]="<name> <file>:Export backup to a .tar file"
+  [delete]="<name>:Delete backup"
+
+  [compact]=":Compact the repository"
+  [prune]=":Prune old backups"
+
+  [break-lock]=":Break the repository lock !USE WITH CAUTION!"
+)
+
+commands+=([borg]=":Manage Backup and Restore operations")
+cmd_borg() {
+  local command="$1"
+
+  if [[ ! " ${!borg_commands[@]} " =~ " $command " ]]; then
+    print_help "borg " "borg_commands"
+    if ! [[ -z "$command" ]]; then
+      echo
+      echo "Unknown command: borg $command"
+    fi
+    exit 1
+  fi
+
+  borg_check
+
+  cd $SERVICE_DIR
+  shift # remove first argument ("borg" command)
+  borg_$command "$@"
+}
+
+borg_check() {
+  # Check if borg is installed
+  if ! command -v borg &>/dev/null; then
+    echo "[BORG] Borg is not installed"
+    # try to install with apt
+    if command -v apt &>/dev/null; then
+      echo "[BORG] Installing borg with apt"
+      sudo apt update
+      sudo apt install -y borgbackup
+    else
+      echo "[BORG] Please install borg manually"
+      exit 1
+    fi
+  fi
+
+  # Check if BORG_REPO, BORG_RSH and BORG_PASSPHRASE are set
+  if [ -z "$BORG_REPO" ]; then
+    echo "[BORG] BORG_REPO is not set"
+    exit 1
+  fi
+  if [ -z "$BORG_RSH" ]; then
+    echo "[BORG] BORG_RSH is not set"
+    exit 1
+  fi
+  if [ -z "$BORG_PASSPHRASE" ]; then
+    echo "[BORG] BORG_PASSPHRASE is not set"
+    exit 1
+  fi
+}
+
+source "$BORG_DIR/borg_commands.sh"
---
+++ borg_commands.sh
@@ -0,0 +1,94 @@
+borg_check_name() {
+  name="$1"
+  if [ -z "$name" ]; then
+    echo "[BORG] Backup name is required"
+    exit 1
+  fi
+}
+
+borg_init() {
+  # ceck if repository exists
+  if borg info &>/dev/null; then
+    echo "[BORG] Repository already exists"
+    exit 0
+  fi
+
+  echo "[BORG] Creating remote repository"
+  borg init --encryption=repokey-blake2 --make-parent-dirs >/dev/null
+  echo "[BORG] Repository created"
+}
+
+borg_info() {
+  echo "[BORG] Repository information:"
+  borg info
+}
+
+borg_list() {
+  echo "[BORG] List of backups:"
+  borg list
+}
+
+borg_backup() {
+  borg_check_name "$1"
+  # TODO allow "LATEST" and auto name the image
+
+  echo "[BORG] Backup current data..."
+  borg create --stats --progress --compression zlib "::$name" ./volumes
+  echo "[BORG] Backup finished"
+}
+
+borg_restore() {
+  borg_check_name "$1"
+  # TODO allow "LATEST" and load the latest backup
+
+  echo "[BORG] Restore data from backup..."
+  borg extract --progress "::$name"
+  echo "[BORG] Restore finished"
+}
+
+borg_export() {
+  borg_check_name "$1"
+  local file="$2"
+
+  if [ -z "$file" ]; then
+    echo "[BORG] File name is required"
+    exit 1
+  fi
+  if [[ ! "$file" =~ \.tar$ ]]; then
+    echo "[BORG] File name must end with .tar"
+    exit 1
+  fi
+
+  echo "[BORG] Export backup to a .tar file..."
+  borg export-tar --progress "::$name" $file
+  echo "[BORG] Export finished"
+}
+
+borg_delete() {
+  borg_check_name "$1"
+
+  echo "[BORG] Delete backup..."
+  borg delete  --progress "::$name"
+  echo "[BORG] Backup deleted"
+}
+
+borg_compact() {
+  echo "[BORG] Compact the repository..."
+  borg compact --progress
+  echo "[BORG] Repository compacted"
+}
+
+borg_prune() {
+  echo "[BORG] Prune old backups..."
+  borg prune --progress --keep-hourly=48 --keep-daily=21 --keep-weekly=16 --keep-monthly=12 --keep-yearly=3 # todo keep last hours
+  echo "[BORG] Old backups pruned"
+}
+
+borg_break-lock() {
+  echo "[BORG] Free the repository lock"
+  echo "[BORG] Waiting 5 seconds before breaking the lock"
+  echo "[BORG] ONLY USE THIS COMMAND IF YOU KNOW WHAT YOU ARE DOING"
+  sleep 5
+  borg break-lock
+  echo "[BORG] Repository lock freed"
+}
