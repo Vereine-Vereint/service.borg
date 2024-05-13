146ad2283b05b0217ad06a3c888c4e99e1fa8077
Emergency1999 <johannes@schmid-company.de>
2024-05-13 19:24:52 +0200

    added logging statements to activate / deactivate

--- borg_commands.sh
+++ borg_commands.sh
@@ -175,12 +175,16 @@ borg_pwgen() {
 
 borg_activate() {
   echo "[BORG] Activating automatic hourly backups for this service..."
-  (crontab -l ; echo "0 * * * * (~/services.connecta/$SERVICE_NAME/service.sh borg backup auto) 2>&1 | logger -t cron_$SERVICE_NAME") | crontab -
+  (crontab -l ; echo "0 * * * * ($SERVICE_DIR/service.sh borg backup auto) 2>&1 | logger -t cron_$SERVICE_NAME") | crontab -
+  echo "[BORG] Following services will be backed up:"
+  echo .
   sudo tail -n +4 /var/spool/cron/crontabs/conny
 }
 
 borg_deactivate() {
   echo "[BORG] Deactivating automatic hourly backups for this service..."
   crontab -l | grep -v "$SERVICE_NAME/service.sh" | crontab -
+  echo "[BORG] Following services will be backed up:"
+  echo .
   sudo tail -n +4 /var/spool/cron/crontabs/conny
 }
